19bb6cc1e56d9aa708de06deb1deee106811df50
Eibel S Mathew <68593354+eibelsmathew@users.noreply.github.com>
2025-12-29 09:35:34 +0530

    Time: 6 ms (11.83%) | Memory: 22.1 MB (90.28%) - LeetSync

--- linked-list/344-reverse-string/reverse-string.py
+++ linked-list/344-reverse-string/reverse-string.py
@@ -1,8 +1,11 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         n = len(s)
-        for i in range(int(len(s)/2)):
-            s[i], s[n-1] = s[n-1], s[i]
-            n = n-1
+        for i in range(len(s)//2):
+            temp = None
+            if s[i]!=s[n-1-i]:
+                temp = s[i]
+                s[i] = s[n-1-i]
+                s[n-1-i] = temp
         return s
         
